b3ff4155ca7f9383cc191c3424198cafe202c7de
DrunkMaji <97950218+DrunkMaji@users.noreply.github.com>
2022-07-13 20:53:36 +0100

    POM
    implemented POM for various selectors

--- test/pageobjects/banner.ts
+++ test/pageobjects/banner.ts
@@ -1,7 +1,28 @@
-class banner {
-  get searchbtn() {
-    return $('button.ii-1nx0ar8')
-  }  
+class banner {                              //create class for selectors 
+  get searchbtn() {                         //block defining a const
+    return $('button.ii-1nx0ar8')           //selector - specifically the search button
+  } 
+  
+  get modal() {
+    return $('#chakra-modal--body-5')  // investment search modal 
+  } 
+
+  get OpenATradingAcc() {
+    return $('//footer//a[@href="/ii-accounts/trading-account"]')  //  find child element in 'footer'
+  }
+  
+  get input() {
+    return $('#chakra-modal--body-5').$('input.chakra-input.ii-8530ov') // find input value in child element of modal
+  }
+
+  //get amc() {
+   // return await $$('[id="chakra-modal--body-5"] ul li') // finds list of elements  
+  //}
+
+  
+
+
+
 }
 
-export default new banner();
+export default new banner();                //allow use of class in your code providing its been imported
--- test/specs/interview.ts
+++ test/specs/interview.ts
@@ -1,8 +1,8 @@
 const { default: ChromeDriverService } = require("wdio-chromedriver-service");
 
-import banner from '../pageobjects/banner';
+import banner from '../pageobjects/banner';   //imports class from page object file
 
-describe('ii cookie banner', () => {    //pop up cookie pref modal was interfering with testing, tried multiple methods to bypass (couldnt identify the button and click it as i received the error - another element will be interacted with and trying to use moveTo and browser.click wouldnt work due to an error - browser.click is not a function) but the only one to work was to manualy add the cookie and refresh the pge prior to initiated further tests.
+describe('ii cookie banner', () => {    //pop up cookie pref modal was interfering with testing, tried multiple methods to bypass (couldnt identify the button and click it as i received the error - another element will be interacted with and trying to use moveTo and browser.click wouldnt work due to an error - browser.click is not a function) but the only one to work was to manualy add the cookie and refresh the page prior to initiating further tests.
   
     
      it('cookie banner accept', async() => {     
@@ -17,18 +17,16 @@ describe('ii cookie banner', () => {    //pop up cookie pref modal was interferi
        browser.refresh();  // refresh browser 
 
        //const btnAccept = await $('[role=button]');  // explored different ways of accepting the cookie pref modal, didnt work due to 'other element would receive the click' error
-       //await browser.pause(5000) // added for testing purposes, holds no real use during implementation 
+       //await browser.pause(5000)                    // added for testing purposes, holds no real use during implementation 
        //await btnAccept.moveTo();
        //await btnAccept.click();
        //await browser.pause(5000)
        
       });
 
-     it(' footer links direct correctly', async() => {   
+     it(' footer links direct correctly', async() => {
 
-       const OpenATradingAcc = await $('//footer//a[@href="/ii-accounts/trading-account"]'); // find child element in 'footer'
-      
-       await OpenATradingAcc.click();  // clicks predefined const
+       await banner.OpenATradingAcc.click();  // clicks predefined const
        await browser.pause(5000) // 5 second wait
 
        await expect(browser).toHaveUrlContaining('/ii-accounts/trading-account');  // checks url for ('***')
@@ -37,26 +35,17 @@ describe('ii cookie banner', () => {    //pop up cookie pref modal was interferi
 
      it('investment search modal', async() => {
 
-      
-      
-       //const searchbtn = await $('button.ii-1nx0ar8'); // find element using unique class of button
-       
-       //await searchbtn.click(); //clicks searchbtn to show investment search modal
        await banner.searchbtn.click(); //clicks searchbtn to show investment search modal (POM)
+
        await browser.pause(5000) //forced wait should use watfordisplayed really
-       
-       
-       const modal = await $('#chakra-modal--body-5'); // finds element investment search modal 
-       
-       await expect(modal).toBeDisplayed(); // checks modal is displayed
+                 
+       await expect(banner.modal).toBeDisplayed(); // checks modal is displayed
        
         });
 
      it('navigate to instrument', async() => {
-
-      const input = await $('#chakra-modal--body-5').$('input.chakra-input.ii-8530ov'); // find input value in child element of modal
-      
-      input.addValue('amc'); //types value into search bar
+     
+      banner.input.addValue('amc'); //types value into search bar
 
       await browser.pause(5000)  // just there for demo purposes so i can see whats typed in ^ easier
      
@@ -69,26 +58,17 @@ describe('ii cookie banner', () => {    //pop up cookie pref modal was interferi
          const a = await e.getText(); // gets text of instance of amc
          if (a.includes('AMC Entertainment Holdings Inc Class A')) {   //checks text against specified value 
          // console.log({a},  e.getText()); // used to debuf with console
-          element = e;  // if string is found empty var is defined as selected element
-             
-      
-            
-
-           
+         element = e;  // if string is found empty var is defined as selected element
+              };
             };
-
-        };
         
         await element.click();  // clicks now defined var
       
-     
-      
       await browser.pause(2500)
     
-        
-    });
+     });
 
-  it('test stock page', async() => {   
+     it('test stock page', async() => {   
 
       await expect(browser).toHaveUrlContaining('/amc-entertainment-holdings-inc-class-a/NYSE:AMC');  // checks url for ('***')
  
